66586019fc147cc9462152693162be90fe910c29
nekitoseek <nmamaev_04@mail.ru>
2024-10-14 21:37:05 +0700

    Add files via upload

---
+++ first_lesson/first_lesson/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  first_lesson
+//
+//  Created by Никита Мамаев on 14.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
